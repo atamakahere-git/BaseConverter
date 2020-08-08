c9f5d28c25683c881d26e5ce4b17dbd17a3b27d2
atamakahere <git@atamaka.maskmy.id>
2020-08-08 10:49:50 +0530

    1. Added project config files. 2. Removed operations.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import com.atamaka.*;
-import com.atamaka.opertions.*;
 
 public class Main {
     public static void main(String[] args) {
         Decimal decimal = new Decimal(1906);
         Octal octal = new Octal(2765);
         Binary binary = new Binary(1101);
-        
-
+        System.out.println(binary.toString());
     }
 }
